e83f91089f99c90fddb2a0111148fe0f4f147d1e
thomasandersonx <andersonthomas597@gmail.com>
2017-02-20 13:14:10 +0200

    Create choice.py

---
+++ choice.py
@@ -0,0 +1,11 @@
+print("The time has come to make a choice Neo \n \
+You take the red pill , you stay in github , and i'll show you how deep the rabbit hole \n \
+You take the blue pill , you go back to windows , play minecraft , and chat on facebook \n \
+But remember all i'm offering is awesomeness . ")
+choice = input('-1 Red Pill , -2 Blue Pill')
+if choice == 1:
+ print('Come with me , and create your repository ')
+elif choice ==2: 
+ print('Bye Bye ....')
+else:
+ print("You don't understand do you ")
